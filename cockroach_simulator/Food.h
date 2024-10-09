f34bb679849d05fd647798f79cbb3eabc74dd334
Nurbek Asanbekov <nurbek.asanbekov@mantle3d.com>
2024-10-09 12:55:45 +0600

    code formatting

--- cockroach_simulator/Food.h
+++ cockroach_simulator/Food.h
@@ -6,66 +6,66 @@
 class Cockroach;
 class Food
 {
-public:
-	constexpr static const double DEFAULT_SIZE = 40.0;
-	constexpr static const double PIECE_SIZE   = 0.1;
+    public:
+        constexpr static const double DEFAULT_SIZE = 40.0;
+        constexpr static const double PIECE_SIZE   = 0.1;
 
-	Food(int x, int y) : m_x(x), m_y(y), m_size(DEFAULT_SIZE)
-	{
-	}
+        Food(int x, int y) : m_x(x), m_y(y), m_size(DEFAULT_SIZE)
+    {
+    }
 
-	void eat()
-	{
-		m_size -= PIECE_SIZE;
-	}
+        void eat()
+        {
+            m_size -= PIECE_SIZE;
+        }
 
-	void draw(HDC hdc, std::list<std::shared_ptr<Cockroach>> &cockroaches)
-	{
-		int	 percent	  = (int)(m_size * 100 / DEFAULT_SIZE);
-		auto eaters_count = std::count_if(cockroaches.begin(), cockroaches.end(), [this](auto cockroach) {
-			if (cockroach->get_status() == Cockroach::Status::CONSUMING &&
-				cockroach->distance(m_x, m_y) < cockroach->get_radius() + m_size + 10)
-			{
-				return true;
-			}
-			return false;
-		});
+        void draw(HDC hdc, std::list<std::shared_ptr<Cockroach>> &cockroaches)
+        {
+            int	 percent	  = (int)(m_size * 100 / DEFAULT_SIZE);
+            auto eaters_count = std::count_if(cockroaches.begin(), cockroaches.end(), [this](auto cockroach) {
+                    if (cockroach->get_status() == Cockroach::Status::CONSUMING &&
+                            cockroach->distance(m_x, m_y) < cockroach->get_radius() + m_size + 10)
+                    {
+                    return true;
+                    }
+                    return false;
+                    });
 
-		std::wstring status_str		  = L"food: " + std::to_wstring(percent) + L"%";
-		std::wstring eaters_count_str = L"eaters: " + std::to_wstring(eaters_count);
+            std::wstring status_str		  = L"food: " + std::to_wstring(percent) + L"%";
+            std::wstring eaters_count_str = L"eaters: " + std::to_wstring(eaters_count);
 
-		TextOut(hdc, (int)(m_x + m_size / 2), (int)(m_y - m_size / 2), status_str.c_str(), (int)status_str.length());
-		TextOut(hdc, (int)(m_x + m_size / 2), (int)(m_y - m_size / 2 + 10), eaters_count_str.c_str(),
-			(int)eaters_count_str.length());
+            TextOut(hdc, (int)(m_x + m_size / 2), (int)(m_y - m_size / 2), status_str.c_str(), (int)status_str.length());
+            TextOut(hdc, (int)(m_x + m_size / 2), (int)(m_y - m_size / 2 + 10), eaters_count_str.c_str(),
+                    (int)eaters_count_str.length());
 
-		Rectangle(
-			hdc, (int)(m_x - m_size / 2), (int)(m_y - m_size / 2), (int)(m_x + m_size / 2), (int)(m_y + m_size / 2));
-	}
+            Rectangle(
+                    hdc, (int)(m_x - m_size / 2), (int)(m_y - m_size / 2), (int)(m_x + m_size / 2), (int)(m_y + m_size / 2));
+        }
 
-	inline double get_size()
-	{
-		return m_size;
-	}
-	inline int get_x()
-	{
-		return m_x;
-	}
-	inline int get_y()
-	{
-		return m_y;
-	}
+        inline double get_size()
+        {
+            return m_size;
+        }
+        inline int get_x()
+        {
+            return m_x;
+        }
+        inline int get_y()
+        {
+            return m_y;
+        }
 
-private:
-	double m_size;
-	int	   m_x;
-	int	   m_y;
+    private:
+        double m_size;
+        int	   m_x;
+        int	   m_y;
 };
 
 class FoodFactory
 {
-public:
-	static std::shared_ptr<Food> make_food(int x, int y)
-	{
-		return std::make_shared<Food>(x, y);
-	}
+    public:
+        static std::shared_ptr<Food> make_food(int x, int y)
+        {
+            return std::make_shared<Food>(x, y);
+        }
 };
--- cockroach_simulator/cockroach_simulator.cpp
+++ cockroach_simulator/cockroach_simulator.cpp
@@ -20,39 +20,39 @@ LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY wWinMain(
-	_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+        _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
 
-	// TODO: Place code here.
+    // TODO: Place code here.
 
-	// Initialize global strings
-	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-	LoadStringW(hInstance, IDC_OCKROACHSIMULATOR, szWindowClass, MAX_LOADSTRING);
-	MyRegisterClass(hInstance);
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_OCKROACHSIMULATOR, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
 
-	// Perform application initialization:
-	if (!InitInstance(hInstance, nCmdShow))
-	{
-		return FALSE;
-	}
+    // Perform application initialization:
+    if (!InitInstance(hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
 
-	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_OCKROACHSIMULATOR));
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_OCKROACHSIMULATOR));
 
-	MSG msg;
+    MSG msg;
 
-	// Main message loop:
-	while (GetMessage(&msg, nullptr, 0, 0))
-	{
-		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-	}
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
 
-	return (int)msg.wParam;
+    return (int)msg.wParam;
 }
 
 //
@@ -62,23 +62,23 @@ int APIENTRY wWinMain(
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-	WNDCLASSEXW wcex;
+    WNDCLASSEXW wcex;
 
-	wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.cbSize = sizeof(WNDCLASSEX);
 
-	wcex.style		   = CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc   = WndProc;
-	wcex.cbClsExtra	   = 0;
-	wcex.cbWndExtra	   = 0;
-	wcex.hInstance	   = hInstance;
-	wcex.hIcon		   = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_OCKROACHSIMULATOR));
-	wcex.hCursor	   = LoadCursor(nullptr, IDC_ARROW);
-	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	wcex.lpszMenuName  = MAKEINTRESOURCEW(IDC_OCKROACHSIMULATOR);
-	wcex.lpszClassName = szWindowClass;
-	wcex.hIconSm	   = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+    wcex.style		   = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc   = WndProc;
+    wcex.cbClsExtra	   = 0;
+    wcex.cbWndExtra	   = 0;
+    wcex.hInstance	   = hInstance;
+    wcex.hIcon		   = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_OCKROACHSIMULATOR));
+    wcex.hCursor	   = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName  = MAKEINTRESOURCEW(IDC_OCKROACHSIMULATOR);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm	   = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-	return RegisterClassExW(&wcex);
+    return RegisterClassExW(&wcex);
 }
 
 //
@@ -94,22 +94,22 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-	hInst = hInstance; // Store instance handle in our global variable
+    hInst = hInstance; // Store instance handle in our global variable
 
-	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr,
-		nullptr, hInstance, nullptr);
-	game	  = new Game(hWnd);
-	game->start_game();
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr,
+            nullptr, hInstance, nullptr);
+    game	  = new Game(hWnd);
+    game->start_game();
 
-	if (!hWnd)
-	{
-		return FALSE;
-	}
+    if (!hWnd)
+    {
+        return FALSE;
+    }
 
-	ShowWindow(hWnd, nCmdShow);
-	UpdateWindow(hWnd);
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
 
-	return TRUE;
+    return TRUE;
 }
 
 //
@@ -124,82 +124,82 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	HBITMAP hBtCockroach = 0;
+    HBITMAP hBtCockroach = 0;
 
-	switch (message)
-	{
-	case WM_COMMAND:
-	{
-		int wmId = LOWORD(wParam);
-		// Parse the menu selections:
-		switch (wmId)
-		{
-		case IDM_ABOUT:
-			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-			break;
-		case IDM_EXIT:
-			DestroyWindow(hWnd);
-			break;
-		default:
-			return DefWindowProc(hWnd, message, wParam, lParam);
-		}
-	}
-	break;
-	case WM_PAINT:
-		if (!game->isUpdated())
-		{
-			PAINTSTRUCT ps;
-			RECT		rect;
-			GetClientRect(hWnd, &rect);
+    switch (message)
+    {
+        case WM_COMMAND:
+            {
+                int wmId = LOWORD(wParam);
+                // Parse the menu selections:
+                switch (wmId)
+                {
+                    case IDM_ABOUT:
+                        DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                        break;
+                    case IDM_EXIT:
+                        DestroyWindow(hWnd);
+                        break;
+                    default:
+                        return DefWindowProc(hWnd, message, wParam, lParam);
+                }
+            }
+            break;
+        case WM_PAINT:
+            if (!game->isUpdated())
+            {
+                PAINTSTRUCT ps;
+                RECT		rect;
+                GetClientRect(hWnd, &rect);
 
-			HDC hdc = BeginPaint(hWnd, &ps);
-			BitBlt(hdc, 0, 0, rect.right, rect.bottom, game->getBackbuffDC(), 0, 0, SRCCOPY);
-			EndPaint(hWnd, &ps);
+                HDC hdc = BeginPaint(hWnd, &ps);
+                BitBlt(hdc, 0, 0, rect.right, rect.bottom, game->getBackbuffDC(), 0, 0, SRCCOPY);
+                EndPaint(hWnd, &ps);
 
-			game->update();
-		}
-		break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	case WM_CREATE:
-		//  g_hbmCockroach = LoadBitmap(GetModuleHandle(NULL), MAKEINTRESOURCE(IDB_COCKROACH));
-		//  if (g_hbmCockroach == NULL)
-		//	MessageBox(hWnd, L"Could not load cockroach image!", L"Error", MB_OK | MB_ICONEXCLAMATION);
-		break;
-	case WM_GETMINMAXINFO:
-	{
-		LPMINMAXINFO lp_mmi		 = (LPMINMAXINFO)lParam;
-		lp_mmi->ptMinTrackSize.x = 450;
-		lp_mmi->ptMinTrackSize.y = 350;
-	}
-	break;
-	case WM_CLOSE:
-		game->stop_game();
-		delete game;
-		// break;
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
-	}
-	return 0;
+                game->update();
+            }
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        case WM_CREATE:
+            //  g_hbmCockroach = LoadBitmap(GetModuleHandle(NULL), MAKEINTRESOURCE(IDB_COCKROACH));
+            //  if (g_hbmCockroach == NULL)
+            //	MessageBox(hWnd, L"Could not load cockroach image!", L"Error", MB_OK | MB_ICONEXCLAMATION);
+            break;
+        case WM_GETMINMAXINFO:
+            {
+                LPMINMAXINFO lp_mmi		 = (LPMINMAXINFO)lParam;
+                lp_mmi->ptMinTrackSize.x = 450;
+                lp_mmi->ptMinTrackSize.y = 350;
+            }
+            break;
+        case WM_CLOSE:
+            game->stop_game();
+            delete game;
+            // break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
 }
 
 // Message handler for about box.
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	UNREFERENCED_PARAMETER(lParam);
-	switch (message)
-	{
-	case WM_INITDIALOG:
-		return (INT_PTR)TRUE;
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+        case WM_INITDIALOG:
+            return (INT_PTR)TRUE;
 
-	case WM_COMMAND:
-		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-		{
-			EndDialog(hDlg, LOWORD(wParam));
-			return (INT_PTR)TRUE;
-		}
-		break;
-	}
-	return (INT_PTR)FALSE;
+        case WM_COMMAND:
+            if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+            {
+                EndDialog(hDlg, LOWORD(wParam));
+                return (INT_PTR)TRUE;
+            }
+            break;
+    }
+    return (INT_PTR)FALSE;
 }
